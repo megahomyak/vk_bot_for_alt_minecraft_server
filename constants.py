dd3ee27084f6b59c9329d4e1de385322e3de4a9a
megahomyak <g.megahomyak@gmail.com>
2020-08-18 16:56:58 +0500

    Added normal asynchronous responding and logging (also removed `reply factory`)

--- constants.py
+++ constants.py
@@ -6,6 +6,8 @@ USER_XML_REGISTRATION_DATE_INDEX = 10
 NOT_VALID_USERS_FILENAME = "not_valid_users.txt"
 NOT_VALID_USERS_SEPARATOR = ","
 
+NEW_MESSAGE_EVENT_NAME = "message_new"
+
 VK_GROUP_TOKEN = ""
 VK_GROUP_ID = 123
 
@@ -15,10 +17,18 @@ COMMAND_ADD_NICKNAME = "/wl "
 
 PAGE_NOT_VALID_MESSAGE = (
     "Эта страница невалидна, тебе нельзя попасть на "
-    "сервер! (Я тому, кто написал ник {})"
+    "сервер! (Я тому, кто написал ник {nickname})"
 )
-NICKNAME_ADDED_MESSAGE = "Никнейм {} добавлен в белый список!"
-NICKNAME_IS_NOT_VALID = "Никнейм {} не валидный!"
+NICKNAME_ADDED_MESSAGE = "Никнейм {nickname} добавлен в белый список!"
+NICKNAME_IS_NOT_VALID = "Никнейм {nickname} не валидный!"
+
+VK_ERROR_MESSAGE = "Произошла ошибка при обработке команды {command}"
+LOGGING_ERROR_MESSAGE = "Error {error} while processing command {command}"
+LOGGING_NEW_MESSAGE = "Message from {peer_id}: {text}"
+LOGGING_USER_SERVED = (
+    "User {user_id} with request {command} served with {response}"
+)
+LOGGING_USER_NOT_SERVED = "User {user_id} with request {command} not served"
 
 SERVER_ADDRESS = ""
 SERVER_RCON_PASSWORD = ""
--- main_logic.py
+++ main_logic.py
@@ -1,4 +1,6 @@
 import asyncio
+import functools
+import logging
 
 import aiohttp
 from asyncrcon import AsyncRCON
@@ -8,6 +10,13 @@ import utils
 from rcon_worker import RConWorker
 from vk_page_validators import VKPageValidators
 from vk_worker import VKWorker
+
+logging.basicConfig(
+    filename="bot_errors.log",
+    format="%(asctime)s | %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.DEBUG
+)
 
 
 class MainLogic:
@@ -32,41 +41,83 @@ class MainLogic:
         with open(constants.NOT_VALID_USERS_FILENAME, "w") as f:
             f.write(", ".join(map(str, self.ids_of_not_valid_users)))
 
-    async def start(self):
-        async for message_info in self.vk_worker.listen():
-            peer_id = message_info["peer_id"]
-            text = message_info["text"]
-            reply = self.vk_worker.reply_functions_factory(peer_id)
-            if text.startswith(constants.COMMAND_ADD_NICKNAME):
-                nickname = text[len(constants.COMMAND_ADD_NICKNAME):]
-                if utils.validate_nickname(nickname):
-                    from_id = message_info["from_id"]
-                    if from_id in self.ids_of_not_valid_users:
-                        await reply(
-                            constants.PAGE_NOT_VALID_MESSAGE.format(nickname)
-                        )
-                    else:
-                        sender_page_info = await self.vk_worker.get_page_info(
-                            from_id
-                        )
-                        if await self.vk_page_validators.checks_facade(
-                            sender_page_info
-                        ):
-                            await reply(
-                                constants.NICKNAME_ADDED_MESSAGE.format(
-                                    nickname
-                                )
-                            )
-                        else:
-                            await reply(
-                                constants.PAGE_NOT_VALID_MESSAGE.format(
-                                    nickname
-                                )
-                            )
+    async def handle_message(self, message_info: dict) -> None:
+        peer_id = message_info["peer_id"]
+        text = message_info["text"]
+        from_id = message_info["from_id"]
+        logging.info(
+            constants.LOGGING_NEW_MESSAGE.format(
+                peer_id=peer_id,
+                text=text
+            )
+        )
+        text_for_user = await self.check_command(message_info)
+        if text_for_user:
+            logging.info(
+                constants.LOGGING_USER_SERVED.format(
+                    user_id=from_id,
+                    command=text,
+                    response=text_for_user
+                )
+            )
+            await self.vk_worker.reply(
+                peer_id,
+                text_for_user
+            )
+        else:
+            logging.info(
+                constants.LOGGING_USER_NOT_SERVED.format(
+                    user_id=from_id,
+                    command=text
+                )
+            )
+
+    async def check_command(self, message_info: dict) -> str:
+        text = message_info["text"]
+        if text.startswith(constants.COMMAND_ADD_NICKNAME):
+            nickname = text[len(constants.COMMAND_ADD_NICKNAME):]
+            if utils.validate_nickname(nickname):
+                from_id = message_info["from_id"]
+                if from_id in self.ids_of_not_valid_users:
+                    return constants.PAGE_NOT_VALID_MESSAGE.format(nickname)
+                sender_page_info = await self.vk_worker.get_page_info(
+                    from_id
+                )
+                if await self.vk_page_validators.checks_facade(
+                    sender_page_info
+                ):
+                    return constants.NICKNAME_ADDED_MESSAGE.format(nickname)
                 else:
-                    await reply(
-                        constants.NICKNAME_IS_NOT_VALID.format(nickname)
+                    return constants.PAGE_NOT_VALID_MESSAGE.format(nickname)
+            else:
+                return constants.NICKNAME_IS_NOT_VALID.format(nickname)
+
+    async def coroutine_done_callback(
+            self, future: asyncio.Future, peer_id: int, text: str):
+        future_exception = future.exception()
+        if future_exception:
+            logging.error(
+                constants.LOGGING_ERROR_MESSAGE.format(
+                    error=str(future_exception),
+                    command=text
+                )
+            )
+            await self.vk_worker.reply(
+                peer_id, constants.VK_ERROR_MESSAGE.format(command=text)
+            )
+
+    async def start(self):
+        async for event in self.vk_worker.listen():
+            if event["type"] == constants.NEW_MESSAGE_EVENT_NAME:
+                msg_info = event["object"]["message"]
+                asyncio.gather(
+                    self.handle_message(msg_info)
+                ).add_done_callback(
+                    functools.partial(
+                        self.coroutine_done_callback,
+                        msg_info["peer_id"], msg_info["text"]
                     )
+                )
 
 
 if __name__ == '__main__':
